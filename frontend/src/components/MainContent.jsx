cf1c95839b093991ac6957f0d85243e4ed2a65e8
green tea <hot-green-tea@mail.ru>
2025-09-17 21:15:34 +1200

    css для main content

--- frontend/src/components/MainContent.jsx
+++ frontend/src/components/MainContent.jsx
@@ -1,24 +1,24 @@
 import dog from '/dog.png'
 import dog2 from '/dog2.png'
 import dog3 from '/dog3.png'
+import styles from '../styles/MainContent.module.css';
 
 export default function MainContent() {
     return (
-        <div class="content_1">
-            <div class="slider">
-                <div class="slides">
-                    <div class="slide"><img src={ dog } /></div>
-                    <div class="slide"><img src={ dog2 } /></div>
-                    <div class="slide"><img src={ dog3 } /></div>
-                </div>
-                <button class="button-arrow left"></button>
-                <button class="button-arrow right"></button>
-            </div>
+        <div className={ styles.content }>
+{/*             <div className={ styles.slider }> */}
+{/*                 <div className={ styles.slides }> */}
+                    <img src={ dog } className={ styles.img_dog }/>
+{/*                     <div class="slide"><img src={ dog2 } /></div> */}
+{/*                     <div class="slide"><img src={ dog3 } /></div> */}
+{/*                 </div> */}
+{/*                 <button className={ styles.arr_left }></button> */}
+{/*                 <button className={ styles.arr_right }></button> */}
 
-            <div class="intro_text">
-                <p class="intro_text_1">PROVIDING HIGH-QUALITY PET <span class="pr">PR</span>ODUCTS</p>
-                <p class="intro_text_2">For Orders Call us Today: 985-245-1730</p>
-             </div>
+            <div className={ styles.intro_text }>
+                <p className={ styles.intro_text_1 }>PROVIDING HIGH-QUALITY PET <span className={ styles.pr }>PR</span>ODUCTS</p>
+                <p className={ styles.intro_text_2 }>For Orders Call us Today: 985-245-1730</p>
+            </div>
         </div>
     )
 }
